deb347dd37d5017b47abb5af49bc5dfcc3920456
EitanCegla <48772967+EitanCegla@users.noreply.github.com>
2019-05-04 21:48:43 +0300

    Add files via upload

--- selenium test.py
+++ selenium test.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
 driver = webdriver.Chrome(executable_path='C:/Users/Eitan_Cegla/Desktop/Eitan/DevOps/Class 3/chromedriver.exe')
 driver.get("http://192.168.99.100:5000")
-con_txt = driver.find_element_by_tag_name("body")
-print(con_txt.text)
+content_txt = driver.find_element_by_tag_name("body")
+print(content_txt.text)
