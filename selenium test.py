1af7077b0825b564cfee735acb7adcb32275e370
EitanCegla <eitance@yahoo.com>
2019-05-06 01:58:52 +0300

    name change

--- selenium test.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-driver = webdriver.Chrome(executable_path='C:/Users/Eitan_Cegla/Desktop/Eitan/DevOps/Class 3/chromedriver.exe')
-driver.get("http://192.168.99.100:5000")
-content_txt = driver.find_element_by_tag_name("body")
-print(content_txt.text)
